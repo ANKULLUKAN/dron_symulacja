6d66ff80e2930facd883072cfcb271f0108ad85f
ZetHy <ZetHy@DESKTOP-VV9GUK2>
2025-05-08 15:45:56 +0200

    Dodaj pliki projektów.

---
+++ Projekt_obiektowka/main.cpp
@@ -0,0 +1,230 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <iostream>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+};
+
+struct Mesh {
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    GLuint VAO, VBO, EBO;
+};
+
+struct Node {
+    glm::mat4 transform;
+    std::vector<unsigned int> meshIndices;
+    std::vector<Node> children;
+};
+
+std::vector<Mesh> meshes;
+Node rootNode;
+
+float yaw = 0.0f, pitch = 0.0f;
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+bool leftMousePressed = false;
+float radius = 5.0f;
+
+void scroll_callback(GLFWwindow*, double, double yoffset) {
+    radius -= yoffset;
+    if (radius < 1.0f) radius = 1.0f;
+    if (radius > 20.0f) radius = 20.0f;
+}
+
+void mouse_button_callback(GLFWwindow*, int button, int action, int) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT)
+        leftMousePressed = (action == GLFW_PRESS);
+}
+
+void cursor_position_callback(GLFWwindow*, double xpos, double ypos) {
+    if (!leftMousePressed) {
+        firstMouse = true;
+        return;
+    }
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.2f;
+    yaw += xoffset * sensitivity;
+    pitch += yoffset * sensitivity;
+
+    if (pitch > 89.0f) pitch = 89.0f;
+    if (pitch < -89.0f) pitch = -89.0f;
+}
+
+glm::mat4 aiMatrix4x4ToGlm(const aiMatrix4x4& mat) {
+    return glm::transpose(glm::make_mat4(&mat.a1));
+}
+
+Node processNode(aiNode* ainode) {
+    Node node;
+    node.transform = aiMatrix4x4ToGlm(ainode->mTransformation);
+    for (unsigned int i = 0; i < ainode->mNumMeshes; i++) {
+        node.meshIndices.push_back(ainode->mMeshes[i]);
+    }
+    for (unsigned int i = 0; i < ainode->mNumChildren; i++) {
+        node.children.push_back(processNode(ainode->mChildren[i]));
+    }
+    return node;
+}
+
+bool loadModel(const std::string& path) {
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(path,
+        aiProcess_Triangulate | aiProcess_GenNormals | aiProcess_JoinIdenticalVertices);
+    if (!scene || !scene->HasMeshes()) {
+        std::cerr << "Assimp error: " << importer.GetErrorString() << std::endl;
+        return false;
+    }
+
+    for (unsigned int m = 0; m < scene->mNumMeshes; ++m) {
+        const aiMesh* mesh = scene->mMeshes[m];
+        Mesh myMesh;
+
+        for (unsigned int i = 0; i < mesh->mNumVertices; ++i) {
+            Vertex vertex;
+            vertex.position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+            vertex.normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+            myMesh.vertices.push_back(vertex);
+        }
+
+        for (unsigned int i = 0; i < mesh->mNumFaces; ++i) {
+            const aiFace& face = mesh->mFaces[i];
+            for (unsigned int j = 0; j < face.mNumIndices; ++j)
+                myMesh.indices.push_back(face.mIndices[j]);
+        }
+
+        glGenVertexArrays(1, &myMesh.VAO);
+        glGenBuffers(1, &myMesh.VBO);
+        glGenBuffers(1, &myMesh.EBO);
+
+        glBindVertexArray(myMesh.VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, myMesh.VBO);
+        glBufferData(GL_ARRAY_BUFFER, myMesh.vertices.size() * sizeof(Vertex), myMesh.vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, myMesh.EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, myMesh.indices.size() * sizeof(unsigned int), myMesh.indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(0);
+
+        meshes.push_back(myMesh);
+    }
+
+    rootNode = processNode(scene->mRootNode);
+    return true;
+}
+
+const char* vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+void main() {
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(0.3, 0.4, 1.0, 0.50);
+}
+)";
+
+GLuint createShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    return shader;
+}
+
+void drawNode(const Node& node, const glm::mat4& parentTransform, GLuint shaderProgram) {
+    glm::mat4 globalTransform = parentTransform * node.transform;
+
+    for (unsigned int i : node.meshIndices) {
+        const Mesh& mesh = meshes[i];
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(globalTransform));
+        glBindVertexArray(mesh.VAO);
+        glDrawElements(GL_TRIANGLES, mesh.indices.size(), GL_UNSIGNED_INT, 0);
+    }
+
+    for (const Node& child : node.children) {
+        drawNode(child, globalTransform, shaderProgram);
+    }
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Dron", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+
+    if (!loadModel("E:/projektyCpp/Projekt_obiektowka/x64/Debug/result.gltf")) return -1;
+
+    GLuint vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        float camX = radius * cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        float camY = radius * sin(glm::radians(pitch));
+        float camZ = radius * sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        glm::vec3 cameraPos = glm::vec3(camX, camY, camZ);
+
+        glm::mat4 view = glm::lookAt(cameraPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.f / 600.f, 0.1f, 100.0f);
+
+        glUseProgram(shaderProgram);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+        drawNode(rootNode, glm::mat4(1.0f), shaderProgram);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
